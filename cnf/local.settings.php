953caa936a8b027926b33dcfa03c9a7f2294d604
chris.zietlow <aczietlow@gmail.com>
2015-10-14 14:39:00 -0400

    Adding a settings file.

---
+++ cnf/local.settings.php
@@ -0,0 +1,13 @@
+<?php
+$db_url = 'mysql://default:default@localhost/default';
+$databases = array(
+  'default' => array(
+    'default' => array(
+      'database' => 'default',
+      'username' => 'default',
+      'password' => 'default',
+      'driver' => 'mysql',
+      'host' => 'localhost',
+    ),
+  ),
+);
