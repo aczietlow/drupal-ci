80a006b5fb7e67652aaa7324f20081fec050d917
chris.zietlow <aczietlow@gmail.com>
2015-12-16 16:43:09 -0500

    Updating the build scripts.

--- build/install.sh
+++ build/install.sh
@@ -3,17 +3,7 @@ set -e
 
 path=$(dirname "$0")
 base=$PWD/$path/..
+drush="$base/bin/drush.php $drush_flags -y -r $base/www"
 
-# Pass all arguments to drush. Ripped from https://github.com/craychee/drupal-adonis/blob/master/build/drush-build.sh
-# kudos!
-drush_flags='-y'
-while [ $# -gt 0 ]; do
-  drush_flags="$drush_flags $1"
-  shift
-done
-
-drush="$base/bin/drush.php $drush_flags"
-
-pushd $base/www
 echo "Installing the Drupal."
 $drush si standard --site-name="drupal-ci" --account-pass=admin -y
